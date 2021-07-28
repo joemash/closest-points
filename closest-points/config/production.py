7f9542b72ccc694ddc9971215c0a778b551057bc
josephat <josephat.macharia@savannahinformatics.com>
2021-07-29 01:54:21 +0300

    fix: remove boto storages

--- closest-points/config/production.py
+++ closest-points/config/production.py
@@ -9,13 +9,5 @@ class Production(Common):
     # https://docs.djangoproject.com/en/2.0/ref/settings/#allowed-hosts
     ALLOWED_HOSTS = ["*"]
     INSTALLED_APPS += ("gunicorn", )
-
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/2.0/howto/static-files/
-    # http://django-storages.readthedocs.org/en/latest/index.html
-    INSTALLED_APPS += ('storages',)
-    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-
     MEDIA_URL = '/media/'
 
